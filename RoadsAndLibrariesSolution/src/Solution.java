a6bd5042fce71fe48c46669d1dfd7ea8c9b66a85
zatekoja <atekojazayyad@gmail.com>
2019-11-08 15:00:52 -0500

    Modified logic to be more efficient

--- RoadsAndLibrariesSolution/src/Solution.java
+++ RoadsAndLibrariesSolution/src/Solution.java
@@ -1,31 +1,45 @@
 import java.util.Stack;
 
 public class Solution {
-
     static long roadsAndLibraries(int n, int c_lib, int c_road, int[][] cities) {
+        Graph graph = new Graph(cities, n);
+        int lib = graph.count_trees() * c_lib;
+        int roads = graph.min_amount_of_nodes_to_connect() * c_road;
+        return lib + roads;
+    }
 
-        return 0;
+    public static void main(String[] args) {
+        int[][] matrix = new int[3][3];
+        int c_lib = 2;
+        int c_road = 1;
+        matrix[0][1] = 1;
+        matrix[2][0] = 1;
+        matrix[1][2] = 1;
+        System.out.println(roadsAndLibraries(3, c_lib, c_road, matrix));
     }
 
     //given a possible forest -- two none combined trees
-    //kruskal minimum spanning tree
+    //find edges that dont lead to a cycle
     //run dfs on every node to identify amount of trees -- in count trees function
-    //multiply amount of trees by cost of lib
+    //multiply amount of different trees by cost of lib
     //multiple amount of edges by cost of road
     //add two products to get result
-    class Graph {
-
+    static class Graph {
         int V;
-        int E;
         int[][] adjMatrix;
-
+        //boolean array to monitor if a node is part of another tree
+        boolean[] is_Parent;
+        int[] arr;
         //constructor
-        Graph(int[][] matrix, int vertex, int edge) {
+        Graph(int[][] matrix, int vertex) {
             this.adjMatrix = matrix;
             this.V = vertex;
-            this.E = edge;
+            this.is_Parent = new boolean[V];
+            this.arr = new int[V];
+            for (int i = 0; i < V; i++) {
+                arr[i] = -1;
+            }
         }
-
         //dfs non recur
         void DFS_util(boolean[] visited, int start) {
             Stack<Integer> stack = new Stack<>();
@@ -37,22 +51,25 @@ public class Solution {
                 stack.pop();
                 for (int i = 0; i < adjMatrix[start].length; i++) {
                     if (adjMatrix[start][i] == 1 && !visited[i]) {
+                        start = i;
                         stack.push(adjMatrix[start][i]);
+                        visited[i] = true;
                     }
                 }
             }
         }
 
+        //check : https://www.geeksforgeeks.org/count-number-trees-forest/ for more info.
         int count_trees() {
             //create array of visited
             boolean[] visited = new boolean[V];
             int result = 0;
-
             //For each vertex(goes from 1 till V) run dfs on it
             //dfs will visit every node connected to the starting point and mark its visited to true
             //if a node is not visited after the dfs has been run, then its in another tree in the forest
             for (int i = 0; i < V; i++) {
                 if (!visited[i]) {
+                    is_Parent[i] = true;
                     DFS_util(visited, i);
                     result++;
                 }
@@ -60,12 +77,17 @@ public class Solution {
             return result;
         }
 
-        int minimum_spanning_tree_kruskal() {
-
-
-            return 0;
+        int min_amount_of_nodes_to_connect() {
+            int count = 0;
+            for (int i = 0; i < adjMatrix.length; i++) {
+                for (int j = 0; j < adjMatrix[i].length; j++) {
+                    if (arr[j] == -1 && !is_Parent[j]) {
+                        arr[j] = i;
+                        count++;
+                    }
+                }
+            }
+            return count;
         }
     }
-
-
 }
